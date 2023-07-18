d0700310e8cd835da71e13e7c9ae28a4c12ab2a7
nandosimon@gmail.com <nandosimon@gmail.com>
2023-07-18 14:52:58 -0400

    updating vars

---
+++ vars.tf
@@ -0,0 +1,85 @@
+# Configure the VMware vSphere Provider
+# You can use vcenter login params or simply host esxi login params
+provider "vsphere" {
+  # If you use a domain set your login like this "MyDomain\\MyUser"
+  user           = ""
+  password       = ""
+  vsphere_server = ""
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+#### TEMPLATES
+
+# You must add template in vsphere before use it
+variable "template_image" {
+  default = "centos-7-template-esxi"
+}
+
+#### DC AND CLUSTER
+# Set vpshere datacenter
+variable "dc" {
+  default = "my-litle-datacenter"
+}
+
+# Set cluster where you want deploy your vm
+variable "cluster" {
+  default = "my-litle-cluster"
+}
+
+# Set host where you want deploy your vm
+variable "host" {
+  default = "my-litle-host"
+}
+
+#### GLOBAL NETWORK PARAMS
+# Virtual switch used
+variable "vswitch" {
+  default = "vSwitch0"
+}
+
+variable "dns_servers" {
+  default = ["8.8.8.8", "8.8.4.4"]
+}
+
+#### PARAMS INSTANCES #####################################
+#
+#
+#
+variable "vm_params" {
+  default = {
+    vcpu = "2"
+    ram  = "4096"
+    # You can't set a datastore name with interspace
+    disk_datastore = "datastore_test"
+    disk_size      = "15"
+  }
+}
+
+variable "network_params" {
+  default = {
+    domain        = "test.local"
+    label         = "http_network"
+    vlan_id       = "1"
+    base_address  = "192.168.1."
+    prefix_length = "24"
+    gateway       = "192.168.1.254"
+  }
+}
+
+# Define the root hostname of http instance
+# The root is incrementing for each instance create
+# e.g: a root name with "frt0" will give:
+# frt01 for the first instance
+# ftp02 for the seconde instance etc...
+#
+variable "base_hostname" {
+  default = "frt0"
+}
+
+# Set number of instances
+# WARNING CHANGING THIS VALUE ON STACK ALREADY CREATE WILL BE REMOVE AND RE-CREATE ALL INSTANCES
+variable "vm_desired_capacity" {
+  default = "1"
+}
