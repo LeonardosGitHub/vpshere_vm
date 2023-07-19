8bc38e3e0a095896dc14b5ac6d4cc86731f8d1cd
nandosimon@gmail.com <nandosimon@gmail.com>
2023-07-19 10:35:36 -0400

    updating

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ terraform {
     }
   }
 }
+# Configure the VMware vSphere Provider
+# You can use vcenter login params or simply host esxi login params
+provider "vsphere" {
+  # If you use a domain set your login like this "MyDomain\\MyUser"
+  user           = var.esxi_user
+  password       = var.esxi_password
+  vsphere_server = var.esxi_vsphere_server
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
